f392dc3a80b3ef7b7ee2e13899f1eb8e0f75388e
CathrineC <cjsr.carreon@gmail.com>
2022-09-28 14:46:00 -0700

    Updated Home and Home Test Passing

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -4,9 +4,8 @@ import { NavLink } from 'react-router-dom'
 const Home = () => {
   return (
     <>
-      <h3>Home</h3>
       <h3>Welcome to Apartment App</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>You will find well-appointed apartment homes available for rent based on your preferences. This application will also allow you to post your own listing. </p>
       <div>
       <NavLink to="/apartmentindex">See All Apartments</NavLink>
       </div>
